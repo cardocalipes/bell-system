f3f2104070f27133814c6ba89b920439f3405c10
cardocalipes <cardocalipes@gmail.com>
2023-10-13 13:22:36 +0800

    fixed project root folder

---
+++ pages/custom-sched.vue
@@ -0,0 +1,83 @@
+<template>
+    <li class="back-button"><NuxtLink to ="/Main-Menu/main-menu">Back</NuxtLink></li>
+    <div class="header">
+        Custom
+    </div>
+    <div class="container">
+        <div class="box" onclick="goToSavedSchedulesPage()">
+            <span class="box-icon">&#x1F4C2;</span>
+            <div class="box-label">Saved Schedules</div>
+        </div>
+        <div class="box" onclick="goToAddCustomPage()">
+            <span class="box-icon">&#x2795;</span>
+            <div class="box-label">Add Custom</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+function goToSavedSchedulesPage() {
+            // Redirect to the "Saved Schedules" page
+            window.location.href = "saved-schedules.html";
+        }
+
+        function goToAddCustomPage() {
+            // Redirect to the "Add Custom" page
+            window.location.href = "add-custom.html";
+        }
+</script>
+
+<style scoped>
+body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+        }
+        .back-button {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            padding: 10px 20px;
+            cursor: pointer;
+        }
+        .header {
+            text-align: center;
+            padding: 20px;
+            background-color: #007bff;
+            color: #fff;
+            font-size: 24px;
+        }
+        .container {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            height: 100vh;
+        }
+        .box {
+            text-align: center;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+            cursor: pointer;
+            transition: transform 0.2s;
+            flex: 1;
+            margin: 10px;
+        }
+        .box:hover {
+            transform: scale(1.1);
+        }
+        .box-icon {
+            font-size: 48px;
+            margin-bottom: 10px;
+        }
+        .box-label {
+            font-size: 18px;
+            font-weight: bold;
+        }
+</style>
