608decd2bbbd06a9bcc95e02f4f1396b5782faf0
claraway <chesquaclairecarles@gmail.com>
2023-10-17 15:35:42 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="container">
         <div class="logo">
-            <img src="~/public/images/logo.png" alt="BELLHUB Logo" width="300" height="300">
+            <img src="~/public/images/logo.png" alt="BELLHUB Logo" width="250" height="250">
         </div>
         <div class="login-form">
             <h2>LOGIN</h2>
             <form action="your-login-action.php" method="POST">
                 <input type="text" class="form-input" name="name" placeholder="Name" required>
                 <input type="password" class="form-input" name="access_code" placeholder="Access Code" required>
-                <router-link :to="{ path: '/Main-Menu/main-menu' }"><button type="submit" class="signin-btn">Sign In</button></router-link>
+                <router-link :to="{ path: '/Main-Menu/main-menu' }">
+                <button type="submit" class="signin-btn">Sign In</button></router-link>
             </form>
         </div>
     </div>
@@ -21,35 +22,38 @@
 <style scoped>
 body {
             font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
+            background-color: #ffffff;
         }
         .container {
             text-align: center;
-            margin-top: 100px;
+            margin-top: 40px;
         }
         .logo {
             margin-bottom: 20px;
         }
         .login-form {
-            background-color: #19e36d;
-            padding: 20px;
+            background-color: #b7d9ff;
+            width: 50%;
+            padding: 20px 10px;
             border-radius: 5px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+            margin: 0 auto;
         }
         .form-input {
-            width: 100%;
-            padding: 10px;
+            width: 50%;
+            padding: 10px 20px;
             margin-bottom: 10px;
             border: 1px solid #ccc;
             border-radius: 3px;
         }
         .signin-btn {
-            background-color: #007bff;
+            background-color: #4466fc;
             color: #fff;
             padding: 10px 20px;
             border: none;
             border-radius: 3px;
             cursor: pointer;
+            display: block;
+            margin: 0 auto;
         }
         .signin-btn:hover {
             background-color: #0056b3;
