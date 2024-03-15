f80ccaf9584e6e60e44f753acddc51c0153efb34
carlesclaire <75772909+carlesclaire@users.noreply.github.com>
2024-03-15 12:44:58 +0800

    cc

--- pages/Main-Menu/Custom-Schedule/Add-Custom/add-custom.vue
+++ pages/Main-Menu/Custom-Schedule/Add-Custom/add-custom.vue
@@ -16,7 +16,7 @@
 
             <div class="form-group">
                 <label class="label" for="duration">Duration:</label>
-                <input class="input" v-model="duration" type="time" id="duration">
+                <input class="input" v-model="duration" type="int" id="duration">
             </div>
 
             <div class="form-group">
