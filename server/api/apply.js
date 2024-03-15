7378bbbc9e75eccfc0eb2ca6386c178d878aa69a
cardocalipes <146152840+cardocalipes@users.noreply.github.com>
2024-03-15 15:37:04 +0800

    Database

--- server/api/apply.js
+++ server/api/apply.js
@@ -3,6 +3,18 @@ import prisma from "~/server/db/prisma"
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     
-    return body.startTime;
-    return body.duration;
+    console.log(body);
+
+    
+    const newStartTime = `2000-01-01 ${body.startTime}:00`;
+
+    const newSchedule = await prisma.savedsched.create({
+        data: {
+            schedname: body.schedname,
+            starttime: new Date(newStartTime),
+            duration: body.duration,
+        }
+    });
+
+    return newSchedule;
 });
