51d1e5b18032e2d7247ac8b83a1ad0ed48aba24d
cardocalipes <146152840+cardocalipes@users.noreply.github.com>
2024-03-20 15:57:32 +0800

    sdasd

--- pages/test2.vue
+++ pages/test2.vue
@@ -1,12 +1,18 @@
+
+
 <template>
 <p> Current Time: {{currentDateTime}}</p>
+<p> Set Time: {{setTime}}</p>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import { DateTime } from 'luxon';
+import dayjs from 'dayjs';
 
+const sched1Data = ref(await useFetch("/api/test/aa").data);
 const currentDateTime = ref('');
+let setTime = ref(dayjs(sched1Data.realtime).format("HH:mm"));
 
 onMounted(() => {
   // Update date and time every second
@@ -14,9 +20,8 @@ onMounted(() => {
   // Initial update
   updateDateTime();
 });
-
 function updateDateTime() {
-  const formattedDateTime = DateTime.now().setZone('Asia/Manila').toFormat('yyyy-MM-dd HH:mm:ss');
+  const formattedDateTime = DateTime.now().setZone('Asia/Manila').toFormat('HH:mm:ss');
   currentDateTime.value = formattedDateTime;
 }
 
